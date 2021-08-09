d4bb78dd90872cdbdbda79b8793099431d078a62
Ville-Markus Yli-Suutala <villemarkus.ylisuutala@gmail.com>
2021-08-09 23:40:37 +0300

    Updated demo page

--- src/index.js
+++ src/index.js
@@ -5,7 +5,6 @@ import Minesweeper from './Minesweeper';
 ReactDOM.render(
   <React.StrictMode>
     <Minesweeper width={30} height={16} bombCount={99} />
-    <Minesweeper width={8} height={8} bombCount={8} />
   </React.StrictMode>,
   document.getElementById('root')
 );
